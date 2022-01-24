0935e91cc78100e7727af8663a9495d13b270b49
hikalium <hikalium@hikalium.com>
2022-01-25 00:29:26 +0900

    Impl amazon link shortener

---
+++ popup.js
@@ -0,0 +1,29 @@
+function copyToClipboard(str, mimeType) {
+  document.oncopy = function(event) {
+    event.clipboardData.setData(mimeType, str);
+    event.preventDefault();
+  };
+  document.execCommand('copy', false, null);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const statusDiv = document.getElementById('statusDiv');
+  const urlDiv = document.getElementById('urlDiv');
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    if (tabs.length != 1) {
+      return;
+    }
+    const originalUrl = tabs[0].url;
+    if (originalUrl.startsWith('https://www.amazon.co.jp/')) {
+      const dps = originalUrl.match('dp/.*/');
+      if (!dps || dps.length != 1) {
+        return;
+      }
+      const url = 'https://www.amazon.co.jp/' + dps[0];
+      copyToClipboard(url, 'text/plain;charset=UTF-8');
+      statusDiv.innerText = 'Copied!';
+      urlDiv.innerText = url;
+    }
+  });
+});
