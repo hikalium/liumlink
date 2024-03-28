2727d124314f18314c45b0ec01926dd03acab17f
hikalium <hikalium@hikalium.com>
2024-03-28 10:12:57 +0900

    update

--- bench.js
+++ bench.js
@@ -1,17 +1,29 @@
+function sleep(time) {
+  return new Promise((resolve) => {setTimeout(resolve, time)})
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   const benchButton = document.getElementById('benchButton');
   const benchResultDiv = document.getElementById('benchResultDiv');
   const runBench = async function() {
-    const tabs = [];
+    const tabIdList = [];
     const t0 = performance.now();
     for (let i = 0; i < 10; i++) {
-      const t = chrome.tabs.create({url: 'nothing.html', active: false}).await;
-      tabs.push(t);
+      const t = await chrome.tabs.create({url: 'nothing.html', active: false});
+      tabIdList.push(t.id);
     }
+    for (const tabId of tabIdList) {
+      while (true) {
+        const t = await chrome.tabs.get(tabId);
+        if (t.status === 'complete') {
+          break;
+        }
+        await sleep(1);
+      }
+    };
     const t1 = performance.now();
     const diff = t1 - t0;
     benchResultDiv.innerHTML += `<p>${diff}</p>`
   };
-  document.addEventListener('click', runBench);
+  benchButton.addEventListener('click', runBench);
 });
